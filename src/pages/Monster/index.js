a8fd8c41ac01e5625d569fa703a58e61ea713df2
rfazevedo <rogerio@rogerioazevedo.net>
2025-06-23 22:43:47 -0400

    chore: force deploy

--- src/pages/Monster/index.js
+++ src/pages/Monster/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-// import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Table } from 'antd'
 import api from '../../services/api'
